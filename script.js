39f30d7daea2d9af7a01900489b6a3148d5ad951
Aidan Compton <107843400+AidCompton@users.noreply.github.com>
2022-10-16 09:11:50 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ const choices = ["rock", "paper", "scissors"];
 const winners =[];
 
 function game() {
-    // for (let i=1; i <= 5; i++){
-    //     playRound(i);
-    // }
+    for (let i=1; i <= 5; i++){
+        playRound(i);
+    }
     playRound()
     logWins();
 }
