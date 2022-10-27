ed4c55b916aba270bf17a3f319ec4196ca48da36
Aidan Compton <107843400+AidCompton@users.noreply.github.com>
2022-10-27 20:47:15 +0200

    update

--- script.js
+++ script.js
@@ -1,82 +1,50 @@
-const choices = ["rock", "paper", "scissors"];
-const winners =[];
-
-function game() {
-    // for (let i=1; i <= 5; i++){
-    //     playRound(i);
-    // }
-    playRound();
-    logWins();
-}
-
-function playRound(round) {
-    const playerSelection = playerChoice();
-    const computerSelection = computerChoice();
-    const winner = checkWinner(playerSelection, computerSelection);
-    winners.push(winner);
-    logRound(playerSelection, computerSelection, winner, round);
-}
-function playerChoice() {
-    let input = prompt("Type Rock, Paper, or Scissors");
-    while (input == null) {
-        input = prompt("Type Rock, Paper, or Scissors");
+const spans = document.querySelectorAll('#player span');
+const pc = document.querySelector('#pc');
+const msg = document.querySelector('p');
+var target;
+const clicked = (event) => {
+  if (!document.body.classList.contains('noclick')) {
+    document.body.classList.add('noclick');
+    target = event.target;
+    for (var span of spans) {
+      if (target !== span) {
+        span.classList.add('hidden');
+      }
     }
-    input = input.toLowerCase();
-    let check = validateInput(input)
-    while (check == false) {
-        input = prompt(
-            "Type Rock, Paper, or Scissors only please"
-        );
-        while (input == null) {
-            input = prompt("Type Rock, Paper, or Scissors");
-        }
-        input = input.toLocaleLowerCase();
-        check = validateInput(input);
-    }
-    //console.log(input)
-    return input;
+    var random = parseInt(Math.random() * 3);
+    pc.textContent = spans[random].textContent;
+    pc.dataset.id = spans[random].id;
+    pc.classList.remove('hidden');
+    setTimeout(results, 250);
+  }
+};
+const results = function () {
+  if (target.id == pc.dataset.id) {
+    msg.textContent = 'Draw';
+  }
+  if (target.id == 'rock'    && pc.dataset.id == 'paper'  || 
+      target.id == 'paper'   && pc.dataset.id == 'scissor'|| 
+      target.id == 'scissor' && pc.dataset.id == 'rock') {
+    msg.textContent = 'You lost :(';
+  }
+  if (target.id == 'rock'    && pc.dataset.id == 'scissor' || 
+      target.id == 'paper'   && pc.dataset.id == 'rock'    || 
+      target.id == 'scissor' && pc.dataset.id == 'paper') {
+    msg.textContent = 'You Win!';
+  }
+  msg.classList.remove('hidden');
+  setTimeout(reset, 1500);
 }
-
-function computerChoice() {
-    return choices[Math.floor(Math.random() * choices.length)]
+const reset = function () {
+  for (var span of spans) {
+    span.classList.remove('hidden');
+  }
+  msg.classList.add('hidden');
+  pc.classList.add('hidden');
+  setTimeout(function () {
+    document.body.classList.remove('noclick');
+  }, 500);
 }
-
-function validateInput(choice) {
-    if (choices.includes(choice)) {
-        return true
-    } else {
-        return false
-    }
+for (var span of spans) {
+  span.addEventListener('click', clicked);
 }
-
-function checkWinner(choiceP, choiceC){
-    if (choiceP === choiceC) {
-        return 'Tie';
-    } else if (
-        (choiceP === "rock" && choiceC == "scissors") || 
-        (choiceP === "paper" && choiceC == "rock") || 
-        (choiceP === "scissor" && choiceC == "paper") 
-    ) {
-        return "Player";
-    } else {
-        return "Computer";
-    }
-}
-
-function logWins() {
-    let playerWins = winners.filter((item) => item == "Player").length;
-    let computerWins = winners.filter((item) => item == "Computer").length;
-    let ties = winners.filter((item) => item == "Tie").length;
-    console.log("Results: ");
-    console.log("Player Wins: ", playerWins);
-    console.log("Computer Wins: ", computerWins);
-    console.log("Ties: ", ties)
-}
-
-function logRound(playerChoice, computerChoice, winner, round) {
-    console.log('Round: ', round)
-    console.log('Player Chose: ', playerChoice);
-    console.log('Computer chose: ', computerChoice);
-    console.log('Game goes to: ', winner);
-}
-
